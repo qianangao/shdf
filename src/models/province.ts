6e3d10d2a7e58da139c42f3a698afc1da3dfca01
段靖宇 <duanjingyu@xin.com>
2021-01-27 14:59:55 +0800

    feat: 新增省市组件、组织选择组件 build: 更新代理配置

---
+++ src/models/province.ts
@@ -0,0 +1,71 @@
+import { getProvinceData } from '@/services/province';
+
+const findData = (idArr, data) => {
+  if (idArr.length === 1) {
+    return { children: data };
+  }
+
+  let tempData = { children: data };
+  let tempId = idArr[0];
+
+  for (let i = 1; i < idArr.length; i++) {
+    tempId = `${tempId}-${idArr[i]}`;
+    // eslint-disable-next-line no-loop-func
+    tempData = tempData.children.find(item => item.value === tempId);
+    if (!tempData) break;
+  }
+
+  return tempData;
+};
+
+const Model = {
+  namespace: 'globalProvince',
+  state: {
+    provinceData: [],
+  },
+  effects: {
+    *getData({ payload }, { call, put, select }) {
+      const provinceData = yield select(state => state.globalProvince.provinceData);
+      const idArr = payload.id.split('-');
+      const selectData = findData(idArr, provinceData);
+
+      if (selectData && selectData.children && selectData.children.length > 0) {
+        return;
+      }
+
+      selectData.loading = true;
+      const response = yield call(getProvinceData, payload);
+
+      if (!response.error) {
+        selectData.loading = false;
+        selectData.children = yield response.map(item => ({
+          label: item.organizationName,
+          value: item.id,
+          isLeaf: !item.isSubunit,
+        }));
+
+        if (!selectData.value) {
+          yield put({
+            type: 'save',
+            payload: {
+              provinceData: selectData.children,
+            },
+          });
+        } else {
+          yield put({
+            type: 'save',
+            payload: {
+              provinceData: [...provinceData],
+            },
+          });
+        }
+      }
+    },
+  },
+  reducers: {
+    save(state, { payload }) {
+      return { ...state, ...payload };
+    },
+  },
+};
+export default Model;
